dc902cb06d3c708b987f2ac889761921a5fde140
Michael R. Bechtel <mikebec20@gmail.com>
2017-10-22 11:44:50 -0400

    adding react-materialize cdn to html file, updated container, row and column components

--- src/components/Grid/Col.js
+++ src/components/Grid/Col.js
@@ -1,6 +1,6 @@
 import React from "react";
 
 export const Col = ({ size, children }) =>
-  <div className={size.split(" ").map(size => "col-" + size).join(" ")}>
+  <div className={size.split(" ").map(size => "col " + size).join(" ")}>
     {children}
   </div>;
