edd6847f3ee1791e467a9949c179cdf6359c5d34
Ruby Dease <rubydease@me.com>
2017-10-22 21:32:10 -0400

    homepage

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -4,11 +4,11 @@ import Login from "../../components/Login";
 import Join_now from "../../components/Join_now";
 import Footer from "../../components/Footer";
 
-Const Homepage = () =>
+Const homepage = () =>
 <div> 
     <Hero MediaBox src="../../images/working-it.mp4" caption="A demo media box1" width="1200"/>
     </Hero>
-<Container style={{ marginTop: 30 }}>
+ <Container style={{ marginTop: 30 }}>
     <Row>
         <Col size="md-12">
           <h1>Welcome To BAo Bao Book!</h1>
@@ -26,12 +26,13 @@ Const Homepage = () =>
             mi sollicitudin turpis semper, et interdum risus lobortis.
           </p>
           <div class="row center">
-          <a href="#" id="download-button" class="btn-large waves-effect waves-light blue-grey darken-3">Login</a>
-          <a href="#" id="download-button" class="btn-large waves-effect waves-light transparent grey-text">Join now</a>
-        </div>
+            <a href="#" id="download-button" class="btn-large waves-effect waves-light blue-grey darken-3">Login</a>
+            <a href="#" id="download-button" class="btn-large waves-effect waves-light transparent grey-text">Join now</a>
+          </div>
         </Col>
     </Row>
-</Container>
+ </Container>
+
 </div>;
 
 export default Homepage;
