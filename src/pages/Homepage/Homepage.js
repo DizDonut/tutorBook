ab68623ae31392be17aa4357d0e9944ea287e784
Michael R. Bechtel <mikebec20@gmail.com>
2017-10-23 20:12:19 -0400

    add react-materialize modules, removed Footer component, cleaned up fixed export on index files for rest of components

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -1,35 +1,36 @@
 import React, { Component } from "react";
-import Navbar from "../../components/Navbar";
+import Nav from "../../components/Navbar";
 import Login from "../../components/Login";
 import Join_now from "../../components/Join_now";
-import Footer from "../../components/Footer";
+import { Card, CardTitle, Container, Row, Col } from "react-materialize";
+// import { Col, Container, Row } from "../../components/Grid"
 
-Const homepage = () =>
-<div> 
-    <Hero MediaBox src="../../images/working-it.mp4" caption="A demo media box1" width="1200"/>
-    </Hero>
- <Container style={{ marginTop: 30 }}>
+
+const Homepage = () =>
+<div>
+
+ <Container>
+   <Card className="large"
+      header={<CardTitle image="../../../Images/Working-it.mp4.url"><h1>Welcome To Bao Bao Book!</h1></CardTitle>}
+      >
+   </Card>
+
     <Row>
-        <Col size="md-12">
-          <h1>Welcome To BAo Bao Book!</h1>
-        </Col>
-      </Row>
-    <Row>
-        <Col size="md-12">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
-            aliquet diam tortor, id consequat mauris ullamcorper eu. Orci varius
-            natoque penatibus et magnis dis parturient montes, nascetur
-            ridiculus mus. Pellentesque et dui id justo finibus sollicitudin at
-            et metus. Ut feugiat tellus nec metus commodo, sed suscipit nisi
-            gravida. Duis eget vestibulum quam, ut porttitor sem. Donec sagittis
-            mi sollicitudin turpis semper, et interdum risus lobortis.
-          </p>
-          <div class="row center">
-            <a href="#" id="download-button" class="btn-large waves-effect waves-light blue-grey darken-3">Login</a>
-            <a href="#" id="download-button" class="btn-large waves-effect waves-light transparent grey-text">Join now</a>
-          </div>
-        </Col>
+      <Col s={12}>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+          aliquet diam tortor, id consequat mauris ullamcorper eu. Orci varius
+          natoque penatibus et magnis dis parturient montes, nascetur
+          ridiculus mus. Pellentesque et dui id justo finibus sollicitudin at
+          et metus. Ut feugiat tellus nec metus commodo, sed suscipit nisi
+          gravida. Duis eget vestibulum quam, ut porttitor sem. Donec sagittis
+          mi sollicitudin turpis semper, et interdum risus lobortis.
+        </p>
+        <div class="row center">
+          <a href="#" id="download-button" class="btn-large waves-effect waves-light blue-grey darken-3">Login</a>
+          <a href="#" id="download-button" class="btn-large waves-effect waves-light transparent grey-text">Join now</a>
+        </div>
+      </Col>
     </Row>
  </Container>
 
