de6dbeb071d3148db578a0f8e3798b4e93ee905f
AgesilausDrako <agesilaus@yahoo.com>
2017-10-24 12:54:31 -0400

    added base routes and server.js

---
+++ controllers/studentControllers.js
@@ -0,0 +1,37 @@
+const db = require("../models");
+
+// Defining methods for the studentsController
+module.exports = {
+  findAll: function(req, res) {
+    db.Student
+      .find(req.query)
+      .sort({ date: -1 })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  findById: function(req, res) {
+    db.Student
+      .findById(req.params.id)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  create: function(req, res) {
+    db.Student
+      .create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  update: function(req, res) {
+    db.Student
+      .findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  remove: function(req, res) {
+    db.Student
+      .findById({ _id: req.params.id })
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  }
+};
---
+++ routes/api/students.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+const studentsController = require("../../controllers/studentsController");
+
+// Matches with "/api/students"
+router.route("/")
+  .get(studentsController.findAll)
+  .post(studentsController.create);
+
+// Matches with "/api/students/:id"
+router
+  .route("/:id")
+  .get(studentsController.findById)
+  .put(studentsController.update)
+  .delete(studentsController.remove);
+
+module.exports = router;
