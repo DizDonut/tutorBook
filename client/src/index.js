0da43d0b35355a39c6b4fd0742484f20324583f0
AgesilausDrako <agesilaus@yahoo.com>
2017-10-25 12:42:29 -0400

    routed Homepage

--- client/src/index.js
+++ client/src/index.js
@@ -4,4 +4,4 @@ import './index.css';
 import App from './App';
 import Homepage from "./pages/Homepage"
 
-ReactDOM.render(<Homepage />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
