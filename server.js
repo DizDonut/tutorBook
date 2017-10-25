2c526572a85c1f5254f6bfd9bdb0047bc3aded92
Terri Pannell <terripannell@gmail.com>
2017-10-25 11:27:48 -0400

    modified package.json scripts and changed port number in server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // Configure body parser for AJAX requests
 app.use(bodyParser.urlencoded({ extended: false }));
