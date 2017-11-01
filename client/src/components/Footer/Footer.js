5aa62d1a2d055656bd6334362e327952ab8d6fd3
Ruby Dease <rubydease@me.com>
2017-10-31 20:03:23 -0400

    updates

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -5,17 +5,8 @@ export default class footer extends React.Component {
     render(){
       return (
 
-        <Footer copyrights="&Bao Bao Book 2017"  moreLinks={ <a className="grey-text text-lighten-4 right"
-        href="#!">More Links</a>}
-              links={
-                <ul>
-                  <li><a className="grey-text text-lighten-3" href="#!">Link 1</a></li>
-                  <li><a className="grey-text text-lighten-3" href="#!">Link 2</a></li>
-                </ul>
-              }
-              className='light-green'>
-          <h3 className="white-text">Footer Content</h3>
-          <p className="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+        <Footer copyrights="&Bao Bao Book 2017" className='example'>
+          
         </Footer>
       );
     }
